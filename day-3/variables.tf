11b03fa81d97f8af6391d975800681a25d2ef601
Himanshu <himanshupotpose388@gmail.com>
2026-03-01 12:28:55 +0530

    asg.tf

---
+++ day-3/variables.tf
@@ -0,0 +1,16 @@
+variable "image_id" {
+  default     = "ami-019715e0d74f695be"
+}
+
+variable "instance_type" {
+  default     = "t3.micro"
+}
+variable "project" {
+  default     = "cbz"
+}
+variable "key_name" {
+  default     = "hp-key"
+}
+variable "env" {
+  default     = "dev"
+}
